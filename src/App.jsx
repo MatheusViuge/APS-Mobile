637804e8e6e92e785a8306c5f5a3fce4354869a6
Matheus Viuge <matheus.viiuge05@gmail.com>
2024-05-15 16:53:32 -0300

    Componente Home aparencendo, css iniciado e SPA configurada na Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Routes, Route } from 'react-router-dom'
 import './App.css';
+import { Home } from './components/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header>
+        <header className="Header">
           <nav>
             <ul>
               <li><NavLink to="/">Home</NavLink></li>
-              <li><NavLink to="/novo">Criar chamado</NavLink></li>
+              <li><NavLink to="/novo_chamado">Criar chamado</NavLink></li>
               <li><NavLink to="/chamados">Buscar Chamado</NavLink></li>
             </ul>
           </nav>
         </header>
         <main>
           <Routes>
-            <Route />
+            <Route path="/" element={<Home />} />
             <Route />
             <Route />
             <Route />
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export function Home(){
+  return(
+    <>
+      <h1> Sistema de chamados</h1>
+			<p>Página inicial</p>
+    </>
+  )
+}
